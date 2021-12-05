7f13c01637d940cda9be27b407ebf3047f150700
Nourboughanmi <92955143+Nourboughanmi@users.noreply.github.com>
2021-12-05 21:10:33 +0100

    src\

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import HeaderArea from './HeaderArea';
+import MainArea from './MainArea';
+function App(){
+    return (
+      <div className="App">
+      <HeaderArea/>
+      <MainArea/>
+      </div>
+    );
+  }
+ 
+
+
+export default App;
